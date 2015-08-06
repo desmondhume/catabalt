8d34d6ee48c01a35ad642138f0891bee958a4e38
Gregorio Setti <gregorio.setti@gmail.com>
2015-08-06 23:45:06 +0200

    Cat node and random shelves generator

--- catabalt/GameScene.swift
+++ catabalt/GameScene.swift
@@ -9,37 +9,64 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
+	
+	var catNode: SKSpriteNode!
+	var groundNode: SKSpriteNode!
+	var foregroundNode: SKSpriteNode!
+	
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+	}
+	
+	override init(size: CGSize) {
+		super.init(size: size)
+		
+		backgroundColor = SKColor.greenColor()
+		foregroundNode = SKSpriteNode()
+		
+		catNode = SKSpriteNode(imageNamed: "cat.png")
+		catNode!.size = CGSize(width: 50.0, height: 39.0)
+		catNode!.position = CGPointMake(catNode!.size.width / 2 + 10.0, 100.0)
+		catNode!.anchorPoint = CGPointMake(0.5, 0.5)
+		catNode!.physicsBody = SKPhysicsBody(rectangleOfSize: catNode!.size)
+		catNode!.physicsBody!.dynamic = true
+		foregroundNode!.addChild(catNode)
+		
+		groundNode = SKSpriteNode()
+		groundNode!.color = SKColor.brownColor()
+		groundNode!.size = CGSize(width: size.width, height: 10.0)
+		groundNode!.anchorPoint = CGPointMake(0.0, 0.5)
+		groundNode!.position = CGPointMake(0.0, 0.0)
+		groundNode!.physicsBody = SKPhysicsBody(rectangleOfSize: groundNode!.size)
+		groundNode!.physicsBody!.dynamic = false
+		
+		foregroundNode!.addChild(groundNode)
+		
+		generateShelves(10)
+		
+		addChild(foregroundNode)
+	}
+	
+	func generateShelves(amount: Int) {
+		var position = CGPoint(x: 150.0, y: 50.0)
+		
+		for i in 1...amount {
+			var shelfNode = SKSpriteNode(color: SKColor.brownColor(), size: CGSize(width: 100.0, height: 10.0))
+			shelfNode.name = "SHELF"
+			shelfNode.position = position
+			shelfNode.physicsBody = SKPhysicsBody(rectangleOfSize: shelfNode.size)
+			shelfNode.physicsBody!.dynamic = false
+			
+			foregroundNode!.addChild(shelfNode)
+			
+			position.x += 120.0
+			let randomY = arc4random_uniform( UInt32(size.height) + 1 )
+			position.y = CGFloat(randomY)
+		}
+	}
+	
+	override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
     }
-    
-    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch in (touches as! Set<UITouch>) {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
-    }
+	
 }
